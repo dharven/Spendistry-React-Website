8897d83cea4b5c2dba4ad8b8a61c4ccf129c225b
feminasenjaliya <f.q.onali@gmail.com>
2022-03-19 20:59:00 +0530

    Logout Functionality
    Logout Functionality done. clear local storage and session storage

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,12 +1,22 @@
 import React from "react";
-import {
-  Navbar,
-  
-} from "reactstrap";
+import { Navbar } from "reactstrap";
 
 
 
 const Header = () => {
+  
+  const Logout = () => {
+    
+    window.location.href = "/";
+
+if (localStorage.getItem('email') !== null || localStorage.getItem('jwt') !== null ) {
+        localStorage.removeItem('email')
+          localStorage.removeItem('jwt')
+      } else {
+        sessionStorage.removeItem('email');
+       sessionStorage.removeItem('jwt');
+      }
+  }
   
 
   
@@ -16,7 +26,7 @@ const Header = () => {
         <div className="d-lg-block d-none me-5 pe-3">
           <p style={{fontSize: '21px', textAlign: "center", paddingTop: '10px', paddingLeft: '10%'}}> <span style={{color: "orange"}}>Spe</span><span style={{color: "grey"}}>nd</span><span style={{color: "green"}}>istry</span></p>
         </div>
-       <button>Logout</button>
+       <button onClick={Logout}>Logout</button>  
       </div>
       
     
