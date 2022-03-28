fa69f9573f89acfc5f7f24745cca769929c06b14
Om Brahmbhatt <om.brahmbhatt123@gmail.com>
2022-03-29 00:47:04 +0530

    commit

--- src/layouts/FullLayout.js
+++ src/layouts/FullLayout.js
@@ -2,12 +2,65 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 import { Container } from "reactstrap";
+import React, {useState,useEffect} from 'react'
+import axios from 'axios';
+
 
 const FullLayout = () => {
+
+
+  const[loggedInVerify, setLoggedInVerify] = useState(true);
+  const[login, setLogin] = useState(false);
+
+  useEffect(() => {
+
+    const verify = async () => {
+      if (localStorage.getItem('Jwt') !== null ) {
+        var jwt = localStorage.getItem('Jwt')
+      } else {
+        var jwt = sessionStorage.getItem('Jwt')
+      }
+      axios.post('https://cdbd-18-212-22-122.ngrok.io/loggedInVerify',{} ,{
+        headers: {
+          'auth-token':  jwt    
+        }
+      }).then((response) => {
+        console.log(response.data);
+        if(response.data == "User Verified"){
+        setLoggedInVerify(false);
+        } else {
+        setLoggedInVerify(true);
+        setLogin(true);
+        }
+      }).catch((error) => {
+        console.log(error.message);
+        console.log(error.response.data);
+        setLoggedInVerify(true);
+        setLogin(true);
+  
+      })
+    }
+
+    verify();
+  }, []);
+
+  if(login){
+
+    window.location = "/";
+  }
+
+
+
+
   return (
-    <main>
+   <div>
       {/********header**********/}
-      <Header />
+
+      {(() => {
+        if (!loggedInVerify) {
+          return (
+            <main>
+            <Header />
       <div className="pageWrapper d-lg-flex">
         {/********Sidebar**********/}
         <div className="sidebarArea shadow" id="sidebarArea">
@@ -21,7 +74,21 @@ const FullLayout = () => {
           </Container>
         </div>
       </div>
-    </main>
+      </main>
+
+            
+          )
+        } else {
+          // window.location = "/"
+          // <></>
+          return (
+            <div>
+              <h1>loading</h1>
+            </div>
+          )
+        }
+      })()}
+      </div>
   );
 };
 
